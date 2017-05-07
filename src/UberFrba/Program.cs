2b03105369e56a8ec7d6822ff788f0ff2938fb97
gaboxxz <gabrielbori@hotmail.com>
2017-05-07 12:25:54 -0300

    Arreglando codigo

--- src/UberFrba/Program.cs
+++ src/UberFrba/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using UberFrba.Model;
+
 namespace UberFrba
 {
     static class Program
@@ -16,7 +16,7 @@ namespace UberFrba
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MenuPrincipal());
+            Application.Run(new Form1());
         }
     }
 }
